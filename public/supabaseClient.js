8a2d379a3883e6d15db675f137f867d77169fb23
lijack1980j-beep <lijack1980j@gmail.com>
2026-02-03 00:30:03 +0100

    Update supabaseClient.js

--- public/supabaseClient.js
+++ public/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://YOUR_PROJECT_ID.supabase.co", // SUPABASE_URL
-  "PUBLIC_ANON_KEY"                      // anon key ONLY
+  "https://ntupihuarkcwzkpccldk.supabase.co", // SUPABASE_URL
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dXBpaHVhcmtjd3prcGNjbGRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNjMzOTksImV4cCI6MjA4NTYzOTM5OX0.06aW6X0Azpj3iXyCKOnJEWnE5R19WE5awc6u0iXeeSU"                      // anon key ONLY
 );
